6bafa0ee82a327c2c839f780a1228fb75d0c90ef
Kim, Minho <minho9779@gmail.com>
2022-09-26 15:35:50 +0900

    Main.java 수정 #1

--- src/d201625004/Main.java
+++ src/d201625004/Main.java
@@ -1,8 +1,7 @@
-package d201625004;
-
 public class Main {
     
     public static void main(String[] args) {
-        System.out.println("hello world");
+        String s = "hello world";
+        System.out.println(s);
     }
 }
